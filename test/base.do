e897e4bd815d38b605ac7df023070f5782b47473
Mauricio Caceres <mauricio.caceres.bravo@gmail.com>
2025-10-17 04:57:12 -0400

    Fix aggregation discrepancies between R and Stata

---
+++ test/base.do
@@ -0,0 +1,46 @@
+* stata18-mp -b do test/base.do && mv base.log test/log/base.after.log
+
+local root `c(pwd)'
+cd codes
+do `root'/test/common.do
+
+local methods drimp dripw reg stdipw ipw rc1
+local aggtyps simple group calendar dynamic
+use `root'/tmp/base.dta, clear
+
+    foreach m of local methods {
+        tempfile `m'
+    }
+    foreach m of local methods {
+        disp "`m'"
+        qui csdid Y X, gvar(G) time(period) method(`m') id(id)
+        qui estimates save "``m''.ster", replace
+    }
+
+    foreach m of local methods {
+        mata: `m' = _loadmat("`root'/tmp/tmpbase_`m'")
+        foreach typ of local aggtyps {
+            mata: `m'`typ' = _loadmat("`root'/tmp/tmpbase_`m'_`typ'")
+        }
+    }
+
+    foreach m of local methods {
+        estimates use "``m''.ster"
+        qui csdid
+        mata out    = st_matrix("r(table)")[1::2,1..rows(`m')]'
+        mata errors = colmax(abs(reldif(`m', out)))
+        mata printf("\n%17s: %9.6g\t%9.6g\n", "`m'", errors[1], errors[2])
+        foreach typ of local aggtyps {
+            if "`typ'" == "dynamic" {
+                qui csdid_estat event
+                mata row = st_matrix("r(table)")[1::2,.]'
+                mata row = row[2::rows(row),.]
+            }
+            else {
+                qui csdid_estat `typ'
+                mata row = st_matrix("r(table)")[1::2,.]'
+            }
+            mata errors = colmax(abs(reldif(`m'`typ', row)))
+            mata printf("%17s: %9.6g\t%9.6g\n", "`typ' `m'", errors[1], errors[2])
+        }
+    }
---
+++ test/deb.R
@@ -0,0 +1,50 @@
+# Rscript --no-save --no-restore --verbose test/deb.R > test/log/deb.R.log 2>&1
+
+library(did)
+library(haven)
+library(foreign)
+library(here)
+
+source(file.path(here(), "test/common.R"))
+
+tmp    <- file.path(here(), "tmp")
+deb    <- read_dta(file.path(here(), "../reply_to_DNWZ/data/Deb_etal_20251006_Stata.dta"))
+deb$id <- 1:nrow(deb)
+
+methods <- c("drimp", "dripw", "reg", "stdipw", "ipw", "rc1")
+aggtyps <- c("simple", "group", "calendar", "dynamic")
+kwargs  <- list(data        = deb,
+                yname       = "y",
+                tname       = "t",
+                gname       = "c",
+                idname      = "id",
+                clustervars = "g",
+                panel       = FALSE,
+                cband       = FALSE,
+                bstrap      = FALSE)
+
+results <- list()
+for ( m in methods ) {
+    print(m); results[[m]] <- do.call(att_gt, c(kwargs, list(est_method=m)))
+}
+
+aggres <- list()
+for ( m in methods ) {
+    res <- results[[m]]
+    for ( typ in aggtyps ) {
+        print(c(m, typ)); aggres[[paste(m, typ, sep="_")]] <- aggte(res, type=typ)
+    }
+}
+
+dir.create(tmp)
+for ( m in methods ) {
+    res <- results[[m]]
+    out <- cbind(res$att,  sqrt(diag(as.matrix(res$V_analytical)) /res$n))
+    .savemat(file.path(tmp, paste("tmpout", m, sep="_")), out)
+    for ( typ in aggtyps ) {
+        agg <- aggres[[paste(m, typ, sep="_")]]
+        all <- cbind(agg$overall.att, agg$overall.se)
+        row <- cbind(agg$att.egt,     agg$se.egt)
+        .savemat(file.path(tmp, paste("tmpout", m, typ, sep="_")), rbind(all, row))
+    }
+}
